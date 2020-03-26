85433975410213c3fb91d13409c38a66653df13e
MxJRd <reed182@mail.chapman.edu>
2020-03-26 07:51:46 -0700

    added boilerplate

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ import item from "./item";
 
 import "./index.css";
 
+
 const main = function() {
+  //fetching from thinkful
+  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+
+
+
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
